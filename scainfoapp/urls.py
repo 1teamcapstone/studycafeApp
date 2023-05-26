cb6f5f926825e1fb0dc9e622e3c0c784636f57ac
Boran <boran0926@gmail.com>
2023-05-26 21:31:04 +0900

    별점순, 리뷰순으로 정렬하는 기능

--- scainfoapp/urls.py
+++ scainfoapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('all/', views.test_view, name='test_view'),
     path('hashtag/', views.hashtag_view, name='hashtag'),
+    path('sort_star/', views.sortstar_view, name='sort_star'),
+    path('sort_review/', views.sortreview_view, name='sort_star'),
 ]
--- scainfoapp/views.py
+++ scainfoapp/views.py
@@ -13,6 +13,24 @@ def test_view(request):
     posts=paginator.get_page(page)
     return render(request,'scainfoapp/all.html',{'studycafes':studycafes, 'posts':posts, 'hashtags':hashtags})
 
+def sortstar_view(request):
+    hashtags=Hashtags.objects.all()
+    studycafes=Studycafes.objects
+    sca_list=Studycafes.objects.order_by('-star_rating')
+    paginator=Paginator(sca_list,5)
+    page=request.GET.get('page')
+    posts=paginator.get_page(page)
+    return render(request,'scainfoapp/sort_star.html',{'studycafes':studycafes, 'posts':posts, 'hashtags':hashtags})
+
+def sortreview_view(request):
+    hashtags=Hashtags.objects.all()
+    studycafes=Studycafes.objects
+    sca_list=Studycafes.objects.order_by('-review')
+    paginator=Paginator(sca_list,5)
+    page=request.GET.get('page')
+    posts=paginator.get_page(page)
+    return render(request,'scainfoapp/sort_review.html',{'studycafes':studycafes, 'posts':posts, 'hashtags':hashtags})
+
 
 #hash tag 검색
 def hashtag_view(request):    
