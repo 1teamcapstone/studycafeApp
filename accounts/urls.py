51dde97182c83388c8f1aeef4ef8549616049870
Helios5778K <helios5778k@gmail.com>
2023-05-29 04:58:30 +0900

    위치등록 페이지 디자인 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
-    # path('mypage/', mypage, name='mypage'),
     
 ]
