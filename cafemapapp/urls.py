2961ab94e30346eda3bf7a7a603f31fc676912ca
Boran <boran0926@gmail.com>
2023-05-30 06:04:59 +0900

    스카검색결과 지도 표시 기능, 스카목록 해시태그와 별점 배치 수정

--- cafemapapp/urls.py
+++ cafemapapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+import scainfoapp.views
 
 urlpatterns = [
     #path('backtest/', views.getTestDatas, name='backtest'),
     #path('post/', views.post_view, name='post_view'),
     #path('plz/', views.test_view, name='test_view'),
     path('',views.studycafe_view, name='studycafe_view'),
-    path('<int:pk>/',views.maptest_view, name='maptest_view'),
+    path('<int:pk>/',views.maptest_view, name='maptest_view'),    
+    path('search/',scainfoapp.views.hashtag_view, name='searchmap_view'),
 ]
--- scainfoapp/views.py
+++ scainfoapp/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from cafemapapp.models import Hashtags
 from cafemapapp.models import Search
 from cafemapapp.models import Sort
+from userlocation.models import UserLocation
 
 # Create your views here.
 def test_view(request):
@@ -34,10 +35,6 @@ def test_view(request):
 def sca_detail(request, pk):
     hashtags=Hashtags.objects.all()
     studycafe=get_object_or_404(Studycafes, id=pk)
-    # sca_list=Studycafes.objects.order_by('-star_rating')
-    # paginator=Paginator(sca_list,5)
-    # page=request.GET.get('page')
-    # posts=paginator.get_page(page)
     like = request.POST.get('like_button') # keyword를 입력받음
     #sca_name=Studycafes.objects.filter(id=like).only("name")
     #sca_like=Studycafes.objects.filter(id=like).only("sca_like")
@@ -139,8 +136,7 @@ def hashtag_view(request):
         sort=Sort.objects.get(pk=1)
         print(sort.sort,"db")
         like = request.POST.get('like_button') # keyword를 입력받음
-    #sca_name=Studycafes.objects.filter(id=like).only("name")
-    #sca_like=Studycafes.objects.filter(id=like).only("sca_like")            
+        map = request.POST.get('map_button')          
         if like:        
             item_l=Studycafes.objects.get(pk=like)
             if item_l.sca_like==0:
@@ -153,7 +149,16 @@ def hashtag_view(request):
             else:
                 item_l.sca_like=0
                 item_l.save()
-
+        
+        if map:
+            item=Search.objects.get(pk=1)
+            kwd=item.keyword.split('#')
+            print(kwd,"첫번재 if")
+            kwd2=kwd[1]
+            scaid=Hashtags.objects.filter(name=kwd2).values_list('sca_id')
+            studycafe= Studycafes.objects.filter(id__in = scaid) # 해당 태그를 가진 studycafe 저장
+            location=UserLocation.objects.last()
+            return render(request, 'cafemapapp/searchmap.html',{'studycafe':studycafe, 'location':location})
         
         if kw is not None and st is None:
             item=Search.objects.get(pk=1)
@@ -197,8 +202,7 @@ def hashtag_view(request):
                 studycafes= Studycafes.objects.filter(id__in = scaid) # 해당 태그를 가진 studycafe 저장
                 paginator=Paginator(studycafes,5)
                 page=request.GET.get('page')
-                posts=paginator.get_page(page)
-                
+                posts=paginator.get_page(page)                
 
             if sort.sort=='1':
                 item=Search.objects.get(pk=1)
@@ -218,9 +222,7 @@ def hashtag_view(request):
                 page=request.GET.get('page')
                 posts=paginator.get_page(page)
                 return render(request,'scainfoapp/has_sort_star.html',{'studycafes':studycafes, 'posts':posts, 'hashtags':hashtags})
-
-                
-                
+             
             elif sort.sort=='3':
                 item=Search.objects.get(pk=1)
                 kwd=item.keyword.split('#')
@@ -237,5 +239,3 @@ def hashtag_view(request):
         
 
         return render(request, 'scainfoapp/search_result.html', {'studycafes':studycafes, 'posts':posts, 'keyword':item.keyword, 'hashtags':hashtags})
-
-        
